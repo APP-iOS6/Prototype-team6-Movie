e82bae0c204a4687b04bc189822f4f9078deeed7
gadisom <jeongwon.kim@gmail.com>
2024-08-26 17:42:03 +0900

    initial commit

---
+++ Team6_Movie/Team6_Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team6_Movie
+//
+//  Created by 김정원 on 8/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
