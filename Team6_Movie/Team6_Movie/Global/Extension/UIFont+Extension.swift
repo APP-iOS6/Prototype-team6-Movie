fab65e00f716a8bd08a591cd88e2a11c3620cadb
gadisom <jeongwon.kim@gmail.com>
2024-08-27 10:39:09 +0900

    chore: 폴더 구조화

---
+++ Team6_Movie/Team6_Movie/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+Extension.swift
+//  Team6_Movie
+//
+//  Created by 김정원 on 8/27/24.
+//
+
+import Foundation
+import UIKit
+extension UIFont {
+    static var h0b = UIFont(name: "Pretendard-Black", size: 24) ?? UIFont.systemFont(ofSize: 24, weight: .bold)
+    static var h1b = UIFont(name: "Pretendard-Black", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .bold)
+
+}
