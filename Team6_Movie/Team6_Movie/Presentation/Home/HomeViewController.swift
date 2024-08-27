65e8e1b7e84114f23c46e170e59113322500b5d8
gadisom <jeongwon.kim@gmail.com>
2024-08-27 14:05:16 +0900

    Add: Contents Model

--- Team6_Movie/Team6_Movie/Presentation/Home/HomeViewController.swift
+++ Team6_Movie/Team6_Movie/Presentation/Home/HomeViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        self.title = "홈"
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+#Preview {
+    HomeViewController()
 }
