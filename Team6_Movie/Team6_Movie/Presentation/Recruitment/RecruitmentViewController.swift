5d985eb5ebd8d9c6ecc493dcd661bb775c9b457a
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-08-27 14:16:17 +0900

    [add] : 이미지셋 추가

--- Team6_Movie/Team6_Movie/Presentation/Recruitment/RecruitmentViewController.swift
+++ Team6_Movie/Team6_Movie/Presentation/Recruitment/RecruitmentViewController.swift
@@ -1,29 +1,119 @@
-//
-//  RecruitmentViewController.swift
-//  Team6_Movie
-//
-//  Created by 김정원 on 8/27/24.
-//
-
 import UIKit
 
-class RecruitmentViewController: UIViewController {
+let buttonCategory = ["KBO", "콘서트", "영화", "스포츠", "애니메이션"]
 
+class RecruitmentViewController: UIViewController {
+    let tableView = UITableView(frame: .zero, style: .grouped)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "모두의 씨네"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(CategoryCell.self, forCellReuseIdentifier: "CategoryCell")
 
-        // Do any additional setup after loading the view.
+        tableView.register(PartyCell.self, forCellReuseIdentifier: "CustomCell")
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension RecruitmentViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 5
+        default:
+            return 0
+        }
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! PartyCell
+            cell.imageView?.image = UIImage(systemName: "photo")
+            cell.textLabel?.text = "\(indexPath.row)"
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 100
+        case 1:
+            return 120
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
+
+// MARK: - CategoryCell
+class CategoryCell: UITableViewCell {
+    
+    let stackView: UIStackView
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        stackView = UIStackView()
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        
+        for category in buttonCategory {
+            let button = UIButton(type: .system)
+            button.setTitle(category, for: .normal)
+            stackView.addArrangedSubview(button)
+        }
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            stackView.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("error")
+    }
+}
+
+class PartyCell: UITableViewCell {
+    
+}
+
+#Preview {
+    RecruitmentViewController()
+}
+
